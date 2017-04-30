462fd1757c02c9bcd4b1bd2804ecdfa0d1b57ea6
Anna Mądra <abromba3@gmail.com>
2017-04-30 21:46:24 +0200

    zakomentowanie

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+//zapytanie o dzień tygodnia
 var name = prompt('Jaki dzisiaj jest dzień tygodnia?');
 //alert('Ooooooo, ' + name + ' to piękny dzień na kodzenie !' );
 console.log('Ooooooo, ' + name + ' to piękny dzień na kodzenie !');
+/*
+to jest zadanie 8.4
+*/
